0d4109033d4ca23b13d28d56eaa11f1c02a7f1af
daruan <daruan.rst@gmail.com>
2022-05-23 23:29:48 -0300

    CityService tests

--- src/test/java/com/practice/dragonballcrud/service/CityServiceTest.java
+++ src/test/java/com/practice/dragonballcrud/service/CityServiceTest.java
@@ -2,58 +2,93 @@ package com.practice.dragonballcrud.service;
 
 import com.practice.dragonballcrud.entities.City;
 import com.practice.dragonballcrud.entities.Planet;
+import com.practice.dragonballcrud.repository.CityRepository;
+import com.practice.dragonballcrud.repository.DestroyedCityRepository;
 import com.practice.dragonballcrud.request.CityRequest;
 import com.practice.dragonballcrud.request.PlanetRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
-@WebAppConfiguration
 class CityServiceTest {
 
     @Autowired
-    CityService cityService;
+    private CityService cityService;
+    @Autowired
+    private PlanetService planetService;
 
     @Autowired
-    PlanetService planetService;
+    private CityRepository cityRepository;
 
-    private City testSamples(String name){
-    PlanetRequest testPlanetRequest = new PlanetRequest("Earth", 10, true);
-    Planet testPlanet = planetService.createPlanet(testPlanetRequest);
-    CityRequest testCityRequest = new CityRequest(0,name,10,11,12,testPlanet.getPlanetName());
-    return cityService.createCity(testCityRequest);}
+    @Autowired
+    private DestroyedCityRepository destroyedCityRepository;
+
+    @Autowired
+    private PopulationService populationService;
+
+    @BeforeEach
+    private void setUp() {
+        PlanetRequest testPlanetRequest = new PlanetRequest("Earth", 10, true);
+        Planet testPlanet = planetService.createPlanet(testPlanetRequest);
+        City firstTestCity = cityService
+                .createCity(
+                        new CityRequest(1, "firstTestName", 10L, 1f, 1f, "Earth"));
+
+        City secondTestCity = cityService
+                .createCity(
+                        new CityRequest(2, "secondTestName", 5L, 1f, 1f, "Earth"));
+    }
+
+    @AfterEach
+    void dropDown() {
+        cityRepository.deleteAll();
+    }
 
     @Test
-    void createCity() {
-        City testCity = testSamples("testName");
-        assertEquals("testName", testCity.getCityName());
-        assertEquals(10, testCity.getPopulation());
-        assertEquals(11, testCity.getLongitude());
-        assertEquals(12, testCity.getLatitude());
+    void testCreateCity() {
+        City testCity = cityService
+                .createCity(
+                        new CityRequest(3, "testName", 1L, 1f, 1f, "Earth"));
+
+        City city = cityRepository.findAll().get(2);
+        assertEquals(3, cityRepository.count());
+        assertEquals(testCity.getCityName(), city.getCityName());
+        assertEquals(1, city.getPopulation());
     }
 
 
-
     @Test
-    void destroyCity() {
-        City testCity = testSamples("testName");
-        assertTrue(cityService.cityIsPresent(testCity.getCityId()));
-        cityService.destroyCity(testCity.getCityId());
+    void testDestroyCity() {
+        cityService.destroyCity(1);
+        assertEquals(1, destroyedCityRepository.findAll().get(0).getDestroyedCityId());
     }
 
     @Test
-    void hasPopulationGreaterThan(){
-        City testCity = testSamples("testName");
-        City secondTestCity = testSamples("secondTest");
-        List<City> cities = cityService.hasPopulationGreaterThan(9);
-        assertTrue(!cities.isEmpty());
+    void testHasPopulationGreaterThan() {
+        assertEquals(1, cityService.hasPopulationGreaterThan(5L).size());
+        assertEquals(2, cityService.hasPopulationGreaterThan(4L).size());
+    }
+
+    @Test
+    void testFindByPlanet() {
+        assertEquals(2, cityService.findByPlanet("Earth").size());
+
+    }
+
+    @Test
+    void testFindAll() {
+        assertEquals(2, cityService.findAll().size());
+    }
+
+    @Test
+    void testRemove(){
+        cityService.remove(1);
+        assertEquals(1,cityService.findAll().size());
     }
 }
